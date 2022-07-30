4ad4d12b7abe19d6b818f422722169a2d8d0df4c
mektigboy <mektigboy@protonmail.com>
2022-07-30 13:21:28 -0500

    Create & update Whitelist.sol

---
+++ contracts/Whitelist.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+/// @title Whitelist 
+/// @author Elizao, irwingtello, leandrogavidia, mektigboy
+/// @notice ...
+/// @dev ...
+contract Whitelist {
+}
