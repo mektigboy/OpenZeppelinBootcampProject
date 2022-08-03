858caa5e70aa8b5a3288cea18f92bbec6e739168
mektigboy <mektigboy@protonmail.com>
2022-08-03 11:35:05 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,22 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-deploy");
+require("dotenv").config();
+
 module.exports = {
     solidity: "0.8.15",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        rinkeby: {
+            chainId: 4,
+            url: process.env.RINKEBY_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
 };
