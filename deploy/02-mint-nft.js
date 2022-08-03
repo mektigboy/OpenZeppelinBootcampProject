f20c6d31d97be384fde09c05c8cf71e4b0417866
mektigboy <mektigboy@protonmail.com>
2022-08-03 11:47:11 -0500

    Update deploy/02-mint-nft.js

--- deploy/02-mint-nft.js
+++ deploy/02-mint-nft.js
@@ -3,8 +3,8 @@ const { ethers } = require("hardhat");
 module.exports = async ({ getNamedAccounts }) => {
     const { deployer } = await getNamedAccounts();
     const random = await ethers.getContract("RandomNFTGenerator", deployer);
-    const mintTx = await random.requestObject();
-    const mintTxReceipt = await mintTx.wait(1);
+    const mintTransaction = await random.requestObject();
+    const mintTransactionReceipt = await mintTransaction.wait(1);
 };
 
 module.exports.tags = ["all", "mint"];
