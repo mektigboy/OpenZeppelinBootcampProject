2e8115b2206380f8b4d3a81a5c6dae2bfc612c3b
mektigboy <mektigboy@protonmail.com>
2022-08-03 08:57:05 -0500

    Update

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.15;
 
 /// @title Whitelist 
-/// @author Elizao, irwingtello, leandrogavidia, mektigboy
+/// @author antovanFI Elizao, irwingtello, leandrogavidia, mektigboy
 /// @notice ...
 /// @dev ...
 contract Whitelist {
