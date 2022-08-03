5576e144e398bbf893c861ee4f4f5cae2678e9ff
mektigboy <mektigboy@protonmail.com>
2022-08-03 08:56:56 -0500

    Update contracts/RandomNFTGenerator.sol

---
+++ contracts/RandomNFTGenerator.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+/// @title Random NFT Generator 
+/// @author antovanFI, Elizao, irwingtello, leandrogavidia, mektigboy
+/// @notice Generates NFTs with randomness, and store them in a decentralized way.
+/// @dev This contract utilizes Chainlink VRF v2 for randomness.
+/// URIs point to IPFS.
+/// Imports contracts from OpenZeppelin.
+contract RandomNFTGenerator {
+}
