27892073b942daae7125153dfcacf60ff6655f71
mektigboy <mektigboy@protonmail.com>
2022-08-05 17:49:15 -0500

    Update back-end/contracts/AbitoRandomNFTGenerator.sol

--- back-end/contracts/AbitoRandomNFTGenerator.sol
+++ back-end/contracts/AbitoRandomNFTGenerator.sol
@@ -5,10 +5,16 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+error AbitoRandomNFTGenerator__AlreadyInitialized();
+error AbitoRandomNFTGenerator__NotEnoughETHSent();
+error AbitoRandomNFTGenerator__RangeOutOfScope();
+error AbitoRandomNFTGenerator__TransferFailed();
 
 /// @title AbitoRandomNFTGenerator
 /// @author antovanFI, Elizao, irwingtello, leandrogavidia, mektigboy
@@ -17,98 +23,97 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 /// URIs point to IPFS.
 /// Imports from OpenZeppelin.
 contract AbitoRandomNFTGenerator is
+    AccessControl,
     ERC721URIStorage,
-    VRFConsumerBaseV2,
+    Ownable,
     Pausable,
-    AccessControl
+    VRFConsumerBaseV2
 {
+    // Type Declaration
+    enum Selection {
+        EPIC,
+        RARE,
+        COMMON
+    }
+
     // Whitelist Variables
     using Counters for Counters.Counter;
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
-    // The number of accounts we want to have in our whitelist.
     uint256 public maxNumberOfWhitelistedAddresses;
-
-    // Track the number of whitelisted addresses.
     uint256 public numberOfAddressesWhitelisted;
 
-    // The owner of the contract
-    address owner;
-
-    // To store our addresses, we need to create a mapping that will receive the user's address and return if he is whitelisted or not.
     mapping(address => bool) whitelistedAddresses;
 
-    // Control NFTs in the contract
     uint256 public maxNFTLimit;
     uint256 public maxNFTLimitWhitelist;
     uint256 public nftMinted;
-    /*
-        END WhiteList variables
-    */
 
-    /*
-        Start WhiteList modifiers
-    */
-    modifier verifyWhiteListLimit() {
-        //Se gasta poco gas, y es engorroso porque va ir al minteo publico
-        require(
-            maxNFTLimitWhitelist <= nftMinted,
-            "Sorry go to the public minting!"
-        );
+    // Chainlink VRF Variables
+    VRFCoordinatorV2Interface immutable i_coordinator;
+    uint64 immutable i_subscriptionId;
+    bytes32 immutable i_gasLane;
+    uint32 immutable i_callbackGasLimit;
+    uint16 constant REQ_CONFIRMATIONS = 3;
+    uint32 constant NUM_WORDS = 1;
+
+    // NFT Variables
+    uint256 i_mintFee;
+    uint256 public s_tokenCounter;
+    mapping(uint256 => Selection) private s_tokenIdToBreed;
+    uint256 internal constant MAX_CHANCE = 1000;
+    string[] internal s_tokenURIs;
+    bool s_initialized;
+
+    // VRF Helpers
+    mapping(uint256 => address) public s_requestIdToSender;
+
+    // Events
+    event NFTRequested(uint256 indexed requestId, address requester);
+    event NFTMinted(Selection selection, address minter);
+
+    // Modifiers
+    modifier verifyWhitelistLimit() {
+        require(maxNFTLimitWhitelist <= nftMinted, "Whitelist max. limit.");
         _;
     }
 
     modifier enablePublicMinting() {
-        require(maxNFTLimit <= nftMinted, "Sorry we are empty in stock");
+        require(maxNFTLimit <= nftMinted, "No more stock.");
         require(
             maxNFTLimitWhitelist > nftMinted,
-            "Please wait,whitelist minting are in progress"
+            "Whitelist minting is in progress."
         );
         _;
     }
-    /*
-        END WhiteList modifiers
-    */
-
-    VRFCoordinatorV2Interface immutable i_VRFCoordinator;
-    bytes32 immutable i_gasLane;
-    uint64 immutable i_subscriptionId;
-    uint32 immutable i_callbackGasLimit;
-
-    uint16 constant REQ_CONFIRMATIONS = 3;
-    uint32 constant NUM_WORDS = 1;
-    uint256 constant MAX_CHANCE = 1000;
-
-    mapping(uint256 => address) s_requestIdToSender;
-    string[3] public s_tokenURIs;
-
-    uint256 public s_tokenCounter;
 
     constructor(
         uint256 _maxWhitelistedAddresses,
         uint256 _maxNFTLimit,
         uint256 _maxNFTLimitWhitelist,
-        address VRFCoordinatorV2,
-        bytes32 gasLane,
+        address coordinator,
         uint64 subscriptionId,
+        bytes32 gasLane,
         uint32 callbackGasLimit,
-        string[3] memory tokenURIs
-    ) ERC721("ABITO", "ABI") VRFConsumerBaseV2(VRFCoordinatorV2) {
-        i_VRFCoordinator = VRFCoordinatorV2Interface(VRFCoordinatorV2);
-        i_gasLane = gasLane;
+        string[3] memory tokenURIs, // Set token URIs in the constructor of our contract.
+        uint256 mintFee
+    )
+        ERC721("OpenZeppelin Bootcamp Project", "OBP")
+        VRFConsumerBaseV2(coordinator)
+    {
+        i_coordinator = VRFCoordinatorV2Interface(coordinator);
         i_subscriptionId = subscriptionId;
+        i_gasLane = gasLane;
+        i_mintFee = mintFee;
         i_callbackGasLimit = callbackGasLimit;
-        s_tokenCounter = 0;
-        s_tokenURIs = tokenURIs;
+        _initializeContract(tokenURIs);
 
-        //bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
 
-        //Variables for Whitelist
         maxNumberOfWhitelistedAddresses = _maxWhitelistedAddresses;
         maxNFTLimit = _maxNFTLimit;
         maxNFTLimitWhitelist = _maxNFTLimitWhitelist;
@@ -122,56 +127,39 @@ contract AbitoRandomNFTGenerator is
         _unpause();
     }
 
-    /*
-        Start WhiteList functions
-    */
+    // Whitelist Functions
     function addUserAddressToWhitelist(address _addressToWhitelist)
         public
         onlyRole(DEFAULT_ADMIN_ROLE)
     {
-        // Validate the caller is not already part of the whitelist.
         require(
             !whitelistedAddresses[_addressToWhitelist],
-            "Error: Sender already been whitelisted"
+            "Sender already whitelisted."
         );
-
-        // Validate if the maximum number of whitelisted addresses is not reached. If not, then throw an error.
         require(
             numberOfAddressesWhitelisted < maxNumberOfWhitelistedAddresses,
-            "Error: Whitelist Limit exceeded"
+            "Error: Whitelist limit exceeded."
         );
-
-        // Set whitelist boolean to true.
         whitelistedAddresses[_addressToWhitelist] = true;
-
-        // Increasing the count
         numberOfAddressesWhitelisted += 1;
     }
 
-    // Remove user from whitelist
     function removeUserAddressFromWhitelist(address _addressToRemove)
         public
         onlyRole(DEFAULT_ADMIN_ROLE)
     {
-        // Validate the caller is already part of the whitelist.
         require(
             whitelistedAddresses[_addressToRemove],
-            "Error: Sender is not whitelisted"
+            "Sender is not whitelisted.addmod"
         );
-
-        // Set whitelist boolean to false.
         whitelistedAddresses[_addressToRemove] = false;
-
-        // This will decrease the number of whitelisted addresses.
         numberOfAddressesWhitelisted -= 1;
     }
 
-    // Get the number of whitelisted addresses
     function getNumberOfWhitelistedAddresses() public view returns (uint256) {
         return numberOfAddressesWhitelisted;
     }
 
-    // Get the maximum number of whitelisted addresses
     function getMaxNumberOfWhitelistedAddresses()
         public
         view
@@ -180,32 +168,31 @@ contract AbitoRandomNFTGenerator is
         return maxNumberOfWhitelistedAddresses;
     }
 
-    //Si el usuario no esta en la whitelist, devuelve un false
     function verifyUserAddress(address _whitelistedAddress)
         public
         view
         returns (bool)
     {
-        // Verifying if the user has been whitelisted
         bool userIsWhitelisted = whitelistedAddresses[_whitelistedAddress];
         return userIsWhitelisted;
     }
 
-    // Is the user whitelisted?
     function isWhitelisted(address _whitelistedAddress)
         public
         view
         returns (bool)
     {
-        // Verifying if the user has been whitelisted
         return whitelistedAddresses[_whitelistedAddress];
     }
 
     // Mint a random NFT:
 
     // 1. Get random number.
-    function requestObject() public returns (uint256 requestId) {
-        requestId = i_VRFCoordinator.requestRandomWords(
+    function requestNFT() public payable returns (uint256 requestId) {
+        if (msg.value < i_mintFee) {
+            revert AbitoRandomNFTGenerator__NotEnoughETHSent();
+        }
+        requestId = i_coordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
             REQ_CONFIRMATIONS,
@@ -213,43 +200,46 @@ contract AbitoRandomNFTGenerator is
             NUM_WORDS
         );
         s_requestIdToSender[requestId] = msg.sender;
+        emit NFTRequested(requestId, msg.sender);
     }
 
     // 2. Mint NFT.
-    //Viene directo desde el VRF
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
         internal
         override
     {
-        // Owner of the object.
         address tokenOwner = s_requestIdToSender[requestId];
-        // Asign this NFT a <tokenId>.
         uint256 newTokenId = s_tokenCounter;
         s_tokenCounter = s_tokenCounter + 1;
-        uint256 moddedRng = randomWords[0] % MAX_CHANCE; // Random number generated.
-        uint256 selection = selectFromModdedRng(moddedRng);
+        uint256 moddedRNG = randomWords[0] % MAX_CHANCE;
+        Selection selection = selectionFromModdedRNG(moddedRNG);
         _safeMint(tokenOwner, newTokenId);
-        _setTokenURI(newTokenId, s_tokenURIs[selection]);
+        _setTokenURI(newTokenId, s_tokenURIs[uint256(selection)]);
+        emit NFTMinted(selection, tokenOwner);
     }
 
-    //Mint usuarios whitelist (utilizando modifier para verificar NFTs)
-    //Tiene que ponerse los parametros
-    //---
-    function mintWhiteList(uint256 requestId, uint256[] memory randomWords)
+    function withdraw() public onlyOwner {
+        uint256 amount = address(this).balance;
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        if (!success) {
+            revert AbitoRandomNFTGenerator__TransferFailed();
+        }
+    }
+
+    function mintWhitelist(uint256 requestId, uint256[] memory randomWords)
         public
-        verifyWhiteListLimit
+        verifyWhitelistLimit
     {
-        require(isWhitelisted(msg.sender), "User is not whitelisted");
-        requestObject();
+        require(isWhitelisted(msg.sender), "User is not whitelisted.");
+        requestNFT();
         fulfillRandomWords(requestId, randomWords);
     }
 
-    //Mint usuarios sin whitelist
     function publicMint(uint256 requestId, uint256[] memory randomWords)
         public
         enablePublicMinting
     {
-        requestObject();
+        requestNFT();
 
         fulfillRandomWords(requestId, randomWords);
     }
@@ -262,7 +252,6 @@ contract AbitoRandomNFTGenerator is
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
-    // The following functions are overrides required by Solidity.
     function supportsInterface(bytes4 interfaceId)
         public
         view
@@ -272,47 +261,56 @@ contract AbitoRandomNFTGenerator is
         return super.supportsInterface(interfaceId);
     }
 
-    function getChanceArray() public pure returns (uint256[3] memory) {
+    function setChanceArray() public pure returns (uint256[3] memory) {
         // 0 - 10 = Epic
         // 11 - 100 = Rare
         // 101 - 1000 = Common
         return [10, 100, MAX_CHANCE];
     }
 
-    function selectFromModdedRng(uint256 moddedRng)
+    function selectionFromModdedRNG(uint256 moddedRNG)
         public
         pure
-        returns (uint256)
+        returns (Selection)
     {
         uint256 cumulativeSum = 0;
-        uint256[3] memory chanceArray = getChanceArray();
+        uint256[3] memory chanceArray = setChanceArray();
 
         for (uint256 i = 0; i < chanceArray.length; i++) {
             if (
-                moddedRng >= cumulativeSum &&
-                moddedRng < cumulativeSum + chanceArray[i]
-            ) return i;
-            cumulativeSum = cumulativeSum + chanceArray[i];
+                moddedRNG >= cumulativeSum &&
+                moddedRNG < cumulativeSum + chanceArray[i]
+            ) {
+                return Selection(i);
+            }
+            cumulativeSum = chanceArray[i];
         }
+        revert AbitoRandomNFTGenerator__RangeOutOfScope();
     }
 
-    function getVRFCoordinator()
-        public
-        view
-        returns (VRFCoordinatorV2Interface)
-    {
-        return i_VRFCoordinator;
+    function _initializeContract(string[3] memory tokenURIs) private {
+        if (s_initialized) {
+            revert AbitoRandomNFTGenerator__AlreadyInitialized();
+        }
+        s_tokenURIs = tokenURIs;
+        s_initialized = true;
     }
 
-    function getGasLane() public view returns (bytes32) {
-        return i_gasLane;
+    // Getters:
+
+    function getInitialized() public view returns (bool) {
+        return s_initialized;
     }
 
-    function getSubscriptionID() public view returns (uint64) {
-        return i_subscriptionId;
+    function getMintFee() public view returns (uint256) {
+        return i_mintFee;
     }
 
-    function getCallbackGasLimit() public view returns (uint32) {
-        return i_callbackGasLimit;
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+
+    function getTokenURIs(uint256 index) public view returns (string memory) {
+        return s_tokenURIs[index];
     }
 }
