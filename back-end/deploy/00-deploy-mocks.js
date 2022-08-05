b87696a9b2604baa5c39b335a5fa00cacaa0cae5
mektigboy <mektigboy@protonmail.com>
2022-08-05 12:09:27 -0500

    Update back-end/deploy/00-deploy-mocks.js

--- back-end/deploy/00-deploy-mocks.js
+++ back-end/deploy/00-deploy-mocks.js
@@ -1,20 +1,32 @@
+const {
+    BASE_FEE,
+    GAS_PRICE_LINK,
+} = require("../helper-hardhat-config");
 const { network } = require("hardhat");
 
-const BASE_FEE = "250000000000000000"; // 0.25 ETH
-const GAS_PRICE_LINK = 1e9;
-
-module.exports = async ({ getNamedAccounts, deployments }) => {
-    const { deploy } = deployments;
+module.exports = async ({ deployments, getNamedAccounts }) => {
+    const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
 
     if (chainId == 31337) {
+        log("Local network detected. Deploying mocks...");
+
         await deploy("VRFCoordinatorV2Mock", {
+            args: [BASE_FEE, GAS_PRICE_LINK],
             from: deployer,
             log: true,
-            args: [BASE_FEE, GAS_PRICE_LINK],
         });
+
+        log("Mocks Deployed!");
+        log("-------------------------------------------------");
+        log(
+            "You are deploying to a local network, you will need a local network running to interact with the smart contracts."
+        );
+        log(
+            "Please, run `yarn hardhat console --network localhost` to interact with the deployed smart contracts."
+        );
     }
 };
 
-module.exports.tags = ["all", "mocks"];
+module.exports.tags = ["all", "main", "mocks"];
