b6a6b4950a177a6e309c57c6c71a10ec2e2af8cd
mektigboy <mektigboy@protonmail.com>
2022-08-05 12:12:46 -0500

    Update front-end/src/App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contractABI from "./RandomNFTGenerator.json";
+import { ethers, BigNumber } from "ethers";
+import { useEffect, useState } from "react";
+
+const contractAddress = "0x74eA74c0d471ACB88e979b1c6C24B2dE9F7ac4d3"; // Address of the deployed contract.
 
 function App() {
+  // Connect:
+
+  const [accounts, setAccounts] = useState([]);
+
+  async function connectAccounts() {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccounts(accounts);
+    }
+  }
+
+  useEffect(() => {
+    connectAccounts();
+  }, []);
+
+  // Mint:
+
+  const [mintAmount, setMintAmount] = useState(1);
+
+  async function handleMint() {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        contractAddress,
+        contractABI.abi,
+        signer
+      );
+
+      try {
+        const response = await contract.mint(BigNumber.from(mintAmount)); // Change to name of the function.
+        console.log("response: ", response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {accounts.length && (
+        <div>
+          <button onClick={() => setMintAmount(mintAmount - 1)}>-</button>
+          {mintAmount}
+          <button onClick={() => setMintAmount(mintAmount + 1)}>+</button>
+          <button onClick={handleMint}>Mint</button>
+        </div>
+      )}
     </div>
   );
 }
