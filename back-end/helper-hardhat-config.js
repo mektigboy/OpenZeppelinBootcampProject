0805632c5467cd5f2df3432408bed9da1f5f8f66
mektigboy <mektigboy@protonmail.com>
2022-08-06 08:50:58 -0500

    Update back-end/helper-hardhat-config.js

--- back-end/helper-hardhat-config.js
+++ back-end/helper-hardhat-config.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
+
 const developmentChains = ["hardhat", "localhost"];
 
 const BASE_FEE = "250000000000000000"; // 0.25 LINK
 const GAS_PRICE_LINK = 1e9; // 0.000000001 LINK per gas
 
 const networkConfig = {
+    // Rinkeby
     4: {
         vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
         callbackGasLimit: "500000", // 500,000 gas
@@ -12,11 +15,12 @@ const networkConfig = {
             "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
         mintFee: "10000000000000000", // 0.01 ETH
         name: "rinkeby",
-        subscriptionId: "9747",
+        subscriptionId: process.env.SUBSCRIPTION_ID,
         maxWhitelistedAddresses: "10",
         maxNFTLimit: "20",
         maxNFTLimitWhitelist: "10",
     },
+    // Hardhat - Local
     31337: {
         callbackGasLimit: "500000", // 500,000 gas
         ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
